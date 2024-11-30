b852e32bb4002443ab5347165f1d6a58fdb097f3
Juanan Rodriguez <juananrodriguezmartinez@gmail.com>
2024-11-30 20:09:02 +0100

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import deno from '@deno/astro-adapter';
+
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+	output: 'server',
+	adapter: deno(),
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
 });
