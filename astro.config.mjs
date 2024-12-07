39971fda850a305928c023dc7de9a75b77f910c4
Juanan Rodriguez <juananrodriguezmartinez@gmail.com>
2024-12-08 00:25:42 +0100

    feat: limit gallery by load more button

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import deno from '@deno/astro-adapter';
-
+import fulldev from 'fulldev-ui/integration';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
@@ -11,5 +11,20 @@ export default defineConfig({
 	output: 'server',
 	adapter: deno(),
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(), fulldev({
+		// css: '/src/css/custom.css',
+		colors: {
+			theme: 'dark',
+			dark: {
+				background: '#111110',
+				base: '#6F6D66',
+				brand: '#F50',
+			},
+			light: {
+				background: '#EEEEEC',
+				base: '#6F6D66',
+				brand: '#F50',
+			},
+		},
+	}),],
 });
